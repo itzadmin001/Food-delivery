c8c22212707e1492ad95dd2aa14327a81164a3c9
itzadmin001 <158562740+itzadmin001@users.noreply.github.com>
2025-01-02 17:22:49 +0530

    Food Card complete bro

---
+++ frontend/src/Components/Admin/DashBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashBoard() {
+    return (
+        <div>
+            Kuch mt krna isme bro admin panel ka hai
+        </div>
+    )
+}
+
+export default DashBoard
